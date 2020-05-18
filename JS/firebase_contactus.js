a442570e046ca022a3ea0982f7b8ce7254d5f30c
priyanshusmvs <priyanshusmvs@gmail.com>
2020-05-18 12:10:39 +0530

    Added firestore to Contact Us

--- JS/firebase_contactus.js
+++ JS/firebase_contactus.js
@@ -1,29 +1,36 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCL2v8R6NdJIIXraLLrpIpISbn-xUv5pCo",
-    authDomain: "handyman-1742c.firebaseapp.com",
-    databaseURL: "https://handyman-1742c.firebaseio.com",
-    projectId: "handyman-1742c",
-    storageBucket: "handyman-1742c.appspot.com",
-    messagingSenderId: "728193362142",
-    appId: "1:728193362142:web:a7b38a519c2c106ae8ba4f",
-    measurementId: "G-6DDTBN6137"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  apiKey: "AIzaSyCL2v8R6NdJIIXraLLrpIpISbn-xUv5pCo",
+  authDomain: "handyman-1742c.firebaseapp.com",
+  databaseURL: "https://handyman-1742c.firebaseio.com",
+  projectId: "handyman-1742c",
+  storageBucket: "handyman-1742c.appspot.com",
+  messagingSenderId: "728193362142",
+  appId: "1:728193362142:web:a7b38a519c2c106ae8ba4f",
+  measurementId: "G-6DDTBN6137"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  const database = firebase.firestore();
-    function leave(){
+  const firestore = firebase.firestore();
 
-    var name = document.getElementById('name').value;
-    var mail = document.getElementById('mail').value; 
-    var message = document.getElementById('message').value;
-    alert(message);
-    let data={
-        name:name,
-        message:message
-    }
-    alert(data.name);
-    database.collection(message).doc(mail).set(data);
-    console.log("hh");
-}
+  const form = document.querySelector('#form-message')
+
+  form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	firestore.collection("Feedback").doc(form.mail.value).set({
+        name: form.name.value,
+        email: form.mail.value,
+        message: form.message.value
+    })
+    .then(() => {
+		console.log("Success sent");
+		window.alert("Thank you for your feedback!");
+		})
+    .catch(error => {
+		console.error(error);
+		window.alert(error.message);
+		});
+
+	form.reset();
+  })
